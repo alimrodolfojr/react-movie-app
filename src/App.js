633182e6c450b40a6a997210b88b0032a0b8cf4c
Rodolfo Alim Jr <rodolfoalimjr@Rodolfos-MacBook-Pro-2.local>
2022-05-03 18:05:15 +0800

    React movie app initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+
+
+const API_URL = `http://www.omdbapi.com/?apikey=fd2c6a54`
+
+const movieTitle = 'Batman'
+
 
 function App() {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${movieTitle}`)
+    const data = await response.json()
+    console.log(data)
+  }
+
+   
+  useEffect(() => {
+    searchMovie(movieTitle)
+  },[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
